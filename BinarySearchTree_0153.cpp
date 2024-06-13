6051256a11db685eea2d23e0c17fef9ba85aefe3
Afnan Asilah <afnanyogs@gmail.com>
2024-06-13 12:52:41 +0700

    membuat case 1

--- BinarySearchTree_0153.cpp
+++ BinarySearchTree_0153.cpp
@@ -127,6 +127,21 @@ int main()
 		cout << "5.exit" << endl;
 		cout << "\nEnter your choice (1-5):";
 
+		char ch;
+		cin >> ch;
+		cout << endl;
+
+		switch (ch)
+		{
+		case'1':
+		{
+			cout << "Enter a word:";
+			string word;
+			cin >> word;
+			x.insert(word);
+			break;
+		}
 		
+		}
 	}
 }
