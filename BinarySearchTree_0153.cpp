cb630d3e2ec8cc3ff1939f3fb59dc918998cf733
Afnan Asilah <afnanyogs@gmail.com>
2024-06-13 12:48:02 +0700

    menambahkan method insert

--- BinarySearchTree_0153.cpp
+++ BinarySearchTree_0153.cpp
@@ -26,4 +26,33 @@ public:
 	{
 		ROOT = nullptr; //Inititializing ROOT to null
 	}
+	void insert(string element)// Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, nullptr, nullptr);
+		newNode->info = element;
+		newNode->leftchild = nullptr;
+		newNode->rightchild = nullptr;
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode);
+
+		if (parent == nullptr)
+		{
+			ROOT = newNode;
+			return;
+		}
+
+		if (element < parent->info)
+		{
+			parent->leftchild = newNode;
+		}
+
+		else if (element > parent->info)
+		{
+			parent->rightchild = newNode;
+		}
+	}
+
+
 	
