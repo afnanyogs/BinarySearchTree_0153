6a221dce37b20ad4139859a3500e78606b30e2c6
Afnan Asilah <afnanyogs@gmail.com>
2025-06-26 18:44:21 +0700

    Menampilkan isi pohon secara postorder

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -174,4 +174,10 @@ int main()
             x.preorder(x.ROOT);
             break;
         }
-       
+        case '4':
+        {
+
+            x.postorder(x.ROOT);
+            break;
+        }
+        
