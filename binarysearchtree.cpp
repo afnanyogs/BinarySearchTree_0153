3e116fe095ca3362bbba6205583612d36e244140
Afnan Asilah <afnanyogs@gmail.com>
2025-06-26 18:27:28 +0700

    memperbaiki dari typo

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -26,4 +26,18 @@ public:
         ROOT = nullptr; // initializing ROOT to null
     }
     void search(int element, Node *&parent, Node *&currentNode)
+    {
+        // this function searches the currentNode of the specified Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode - currentNode->rightchild;
+        }
+    }
+
    
