41e2255c83f21442acd4a67e698557445428d367
Afnan Asilah <afnanyogs@gmail.com>
2025-06-26 18:43:57 +0700

    Menampilkan isi pohon secara preorder

--- binarysearchtree.cpp
+++ binarysearchtree.cpp
@@ -168,4 +168,10 @@ int main()
             x.inorder(x.ROOT);
             break;
         }
+        case '3':
+        {
+
+            x.preorder(x.ROOT);
+            break;
+        }
        
